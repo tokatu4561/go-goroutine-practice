b2698ae3dd32351de79e037d37f48ac291600589
tokatu4561 <ryobsk4561@gmail.com>
2022-11-27 13:15:16 +0900

    db 接続

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,22 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
 const webPort = "80"
 
 func main() {
 	// connect to the database
+	db := initDB()
+	db.Ping()
 
 	// create sessions
 
@@ -17,3 +30,54 @@ func main() {
 
 	// listen for web connections
 }
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("cant connect to databse")
+		log.Panic("DB接続エラー")
+	}
+
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	counts := 0
+
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+
+		if err != nil {
+			log.Println("porstgres not yert ready ....")
+			log.Println("準備中 ....")
+		} else {
+			log.Println("conected to database")
+			return connection
+		}
+
+		if counts > 10 {
+			return nil
+		}
+
+		log.Println("Backing, off for 1 secound")
+
+		time.Sleep(1 * time.Second)
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+} 
